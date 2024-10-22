e2eb494c101ea805d6dbc98789fbe6a957c65223
Noel Rappin <noelrap@hey.com>
2024-10-22 08:52:59 -0500

    More ruby

--- app/actions/batch_checkout.rb
+++ app/actions/batch_checkout.rb
@@ -8,16 +8,27 @@ class BatchCheckout
   def process
     data = JSON.load_file(filename)
     data.each do |order_data|
-      line_items = order_data["line_items"].map do |line_item_data|
-        LineItem.new(
-          item: Item.find_by(name: line_item_data["item_name"]),
-          quantity: Integer(line_item_data["quantity"])
-        )
-      end
       CheckoutAction.new.checkout(
-        buyer: Person.find(order_data["buyer_id"]),
-        recipient: order_data["recipient_id"],
-        line_items: line_items
+        buyer: buyer(order_data),
+        recipient: recipient(order_data),
+        line_items: line_items(order_data)
+      )
+    end
+  end
+
+  def buyer(order_data)
+    Person.find(order_data["buyer_id"])
+  end
+
+  def recipient(order_data)
+    order_data["recipient_id"]
+  end
+
+  def line_items(order_data)
+    order_data["line_items"].map do |line_item_data|
+      LineItem.new(
+        item: Item.find_by(name: line_item_data["item_name"]),
+        quantity: Integer(line_item_data["quantity"])
       )
     end
   end
