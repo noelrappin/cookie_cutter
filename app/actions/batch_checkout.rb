79931609d2bfdee882df578653abb84d65939094
Noel Rappin <noelrap@hey.com>
2024-10-30 19:02:22 -0500

    Yard

--- app/actions/batch_checkout.rb
+++ app/actions/batch_checkout.rb
@@ -1,10 +1,12 @@
 class BatchCheckout
   attr_accessor :filename
 
+  # @param filename [String]
   def initialize(filename)
     @filename = filename
   end
 
+  # @return [void]
   def process
     data = JSON.load_file(filename)
     data.each do |order_data|
@@ -16,14 +18,20 @@ class BatchCheckout
     end
   end
 
+  # @param order_data [Hash<String, Any>]
+  # @return [Person]
   def buyer(order_data)
     Person.find(order_data["buyer_id"])
   end
 
+  # @param order_data [Hash<String, Any>]
+  # @return [String]
   def recipient(order_data)
     order_data["recipient_id"]
   end
 
+  # @param order_data [Hash<String, Any>]
+  # @return [Array<LineItem>]
   def line_items(order_data)
     order_data["line_items"].map do |line_item_data|
       LineItem.new(
--- app/actions/checkout_action.rb
+++ app/actions/checkout_action.rb
@@ -1,4 +1,10 @@
 class CheckoutAction
+
+  # @param buyer [Person]
+  # @param recipient [Person]
+  # @param line_items [Array<LineItem>]
+  # @param promotions [Array<String>]
+  # @return [void]
   def checkout(buyer:, recipient:, line_items:, promotions: [])
     ManagePayment.new.manage_payment(buyer, recipient, line_items, promotions)
     Order.create(buyer:, recipient:, line_items:)
