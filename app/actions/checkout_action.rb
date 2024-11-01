3a0a5c2eb6254481f6c8de0eb4a9b62cc0f150be
Noel Rappin <noelrap@hey.com>
2024-11-01 00:55:13 -0500

    Duck Typing

--- app/actions/checkout_action.rb
+++ app/actions/checkout_action.rb
@@ -1,5 +1,6 @@
 class CheckoutAction
   def checkout(buyer:, recipient:, line_items:, promotions: [])
+    raise RuntimeError unless recipient.respond_to?(:address)
     ManagePayment.new.manage_payment(buyer, recipient, line_items, promotions)
     Order.create(buyer:, recipient:, line_items:)
   end
