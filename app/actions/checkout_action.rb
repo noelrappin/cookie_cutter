1995d7d397a5ebb0d30a7bb0741073ddb9662db7
Noel Rappin <noelrap@hey.com>
2024-11-01 00:52:21 -0500

    Type check

--- app/actions/checkout_action.rb
+++ app/actions/checkout_action.rb
@@ -1,5 +1,6 @@
 class CheckoutAction
   def checkout(buyer:, recipient:, line_items:, promotions: [])
+    raise RuntimeError unless recipient.is_a?(Person)
     ManagePayment.new.manage_payment(buyer, recipient, line_items, promotions)
     Order.create(buyer:, recipient:, line_items:)
   end
